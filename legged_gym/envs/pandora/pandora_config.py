8697e053579208b7da87161d4862285f279abef6
Ivan - Pandora Computer <ivanf@vt.edu>
2023-03-20 17:37:07 -0400

    Pandora task now runs, need to make it not crash!

--- legged_gym/envs/pandora/pandora_config.py
+++ legged_gym/envs/pandora/pandora_config.py
@@ -32,7 +32,7 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class PandoraFlatCfg( LeggedRobotCfg ):
     class env( LeggedRobotCfg.env):
-        num_envs = 4096
+        num_envs = 100#4096
         num_observations = 48
         num_actions = 12
 
